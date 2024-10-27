b2b8588ebc689977d6109b5237c62105a4a043c3
sathish <sathishlesa@gmailcom>
2024-10-27 14:54:36 +0530

    MenuItem field name change

--- menu/models.py
+++ menu/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class MenuItem(models.Model):
-    title = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
+    Item_Name = models.CharField(max_length=100)
+    Display_Name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='menu_items/')
 
     def __str__(self):
